b6c95ff1786aac0d15a4908cdcbdd11575c8bcfc
mgrizqy <mgrizqy.work@gmail.com>
2026-03-16 12:12:27 +0700

    chore: project scaffold

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+// Root application module. Wires together all feature modules.
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
+})
+export class AppModule {}
